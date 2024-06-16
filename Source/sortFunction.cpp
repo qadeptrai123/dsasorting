c92481cc796685012eb5139b48609949255be304
qadeptrai123 <23127150@student.hcmus.edu.vn>
2024-06-16 21:26:56 +0700

    test

--- Source/sortFunction.cpp
+++ Source/sortFunction.cpp
@@ -1,3 +1,11 @@
 #include "sortFunction.h"
 #include<iostream>
 
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
--- Source/sortFunction.h
+++ Source/sortFunction.h
@@ -1,2 +1,3 @@
 #pragma once
 
+void quickSort(int arr[], int low, int high);
