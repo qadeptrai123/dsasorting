1da6a2fd9a8f3e3edcc6c4de49ea83cb97656f9f
khuongxuandinh <xuankhuongw@gmail.com>
2024-06-18 17:02:58 +0700

    next sort function

--- Source/sortFunction.cpp
+++ Source/sortFunction.cpp
@@ -41,6 +41,67 @@ void  shellSortWithCounting(int *array, int number_of_elements, int &count_assig
     }
 }
 //Heap Sort
+//https://www.programiz.com/dsa/heap-sort
+void swapElement(int *num1, int *num2) {
+    *num1 = *num1 + *num2;
+    *num2 = *num1 - *num2; //this function requires 3 assignments
+    *num1 = *num1 - *num2;
+}
+void heapify(int array[], int number_of_elements, int current_index) {
+    int index_of_largest_element = current_index;
+    int left = 2 * current_index + 1;
+    int right = 2 * current_index + 2;
+  
+    if (left < number_of_elements && array[left] > array[index_of_largest_element])
+        index_of_largest_element = left;
+    if (right < number_of_elements && array[right] > array[index_of_largest_element])
+        index_of_largest_element = right;
+    if (index_of_largest_element != current_index) {
+        swapElement(&array[current_index], &array[index_of_largest_element]);
+        heapify(array, number_of_elements, index_of_largest_element);
+    }
+}
+  void heapSort(int array[], int number_of_elements) {
+    int i;
+    for (i = number_of_elements / 2 - 1; i >= 0; i--)
+        heapify(array, number_of_elements, i);
+  
+    for (i = number_of_elements - 1; i >= 0; i--) {
+        swapElement(&array[0], &array[i]);
+        heapify(array, i, 0);
+    }
+}
+
+// Heap Sort with counting
+void heapifyWithCounting(int array[], int number_of_elements, int current_index, int &count_assign , int &count_compare) {
+    int index_of_largest_element = current_index; ++count_assign;
+    int left = 2 * current_index + 1; ++count_assign;
+    int right = 2 * current_index + 2; ++count_assign;
+  
+    if ((count_compare += 3) && (left < number_of_elements && array[left] > array[index_of_largest_element]))
+        index_of_largest_element = left; ++count_assign;
+    if ((count_compare += 3) && (right < number_of_elements && array[right] > array[index_of_largest_element]))
+        index_of_largest_element = right; ++count_assign;
+    if ((++count_compare) && (index_of_largest_element != current_index)) {
+        swapElement(&array[current_index], &array[index_of_largest_element]); count_assign += 3;
+        heapifyWithCounting(array, number_of_elements, index_of_largest_element, count_assign, count_compare); ++count_assign;
+    }
+}
+  void heapSortWithCounting(int array[], int number_of_elements, int &count_assign , int &count_compare) {
+    int i;
+    count_assign = 0;
+    count_compare = 0;
+    for (++count_assign, i = number_of_elements / 2 - 1; (++count_compare) && (i >= 0); i--, ++count_assign)
+        heapifyWithCounting(array, number_of_elements, i, count_assign, count_compare); ++count_assign;
+  
+    for (++count_assign, i = number_of_elements - 1; (++count_compare) && (i >= 0); ++count_assign, i--) {
+        swapElement(&array[0], &array[i]); count_assign += 3;
+        heapifyWithCounting(array, i, 0, count_assign, count_compare); ++count_assign;
+    }
+}
+
+
+
 
 
 //Merge Sort
--- Source/sortFunction.h
+++ Source/sortFunction.h
@@ -7,3 +7,8 @@
 
 void shellSort(int *array, int number_of_elements);
 void shellSortWithCounting(int *array, int number_of_elements, int &count_assign , int &count_compare);
+void swapElement(int *num1, int *num2);
+void heapSort(int array[], int number_of_elements);
+void heapify(int array[], int number_of_elements, int current_index);
+void heapifyWithCounting(int array[], int number_of_elements, int current_index, int &count_assign , int &count_compare);
+void heapSortWithCounting(int array[], int number_of_elements, int &count_assign , int &count_compare);
