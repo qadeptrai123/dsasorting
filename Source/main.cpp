3655326622f833939dfe495a1ae3849d645db4f9
qadeptrai123 <23127150@student.hcmus.edu.vn>
2024-06-21 08:34:16 +0700

    21/6 update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,25 @@
 #include "sortFunction.h"
+#include "DataGenerator.h"
+#include<string>
+#include<string.h>
 using namespace std;
 
 
-int main() {
-    int arr[10000] ={0};
-    for (int i = 0; i < 10000; ++i)
-        arr[i] = rand() % 10000;
-    int assign = 0, compare = 0;
-    // shellSortWithCounting(arr, 10000, assign, compare);
-    // heapSortWithCounting(arr, 10000, assign, compare);
-    mergeSortWithCounting(arr, 0, 10000 - 1, assign, compare);
-    cout << endl;
-    cout << "---------" << endl;
-    cout << "Assignments: " << assign << endl;
-    cout << "Comparisons: " << compare << endl;
+int main(int argc, char *argv[]) {
+    int type = strcmp(argv[1], "-a") == 0 ? 1 : 0;
+    //cout << type << "\n";
+    switch (type) {
+        case 1:
+            cout << "ALGORITHM MODE\n";
+                
+            break;
+        case 0:
+            cout << "COMPARISON MODE\n";
+            break;
+        default:
+            cout << "Invalid argument" << endl;
+            break;
+    }
+
     return 0;
 }
