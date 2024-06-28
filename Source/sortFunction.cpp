bad6f8ace0afc7fa4d07add99ca2cdd6ff106ac9
qadeptrai123 <23127150@student.hcmus.edu.vn>
2024-06-28 23:04:28 +0700

    1

--- Source/sortFunction.cpp
+++ Source/sortFunction.cpp
@@ -542,12 +542,12 @@ long long shakerSortWithCounting(int *array, int arraySize) {
 void insertionSort(int *array, int arraySize) {
     for(int i = 1; i < arraySize; ++i) {
         int key = array[i];
-        int j = i - 1;
-        while(j >= 0 && array[j] > key) {
-            array[j+1] = array[j];
-            j--;
+        int j = i;
+        while(j > 0 && array[j-1] > key) {
+            array[j] = array[j-1];
+            --j;
         }
-        array[j+1] = key;
+        array[j] = key;
     }
 }
 
